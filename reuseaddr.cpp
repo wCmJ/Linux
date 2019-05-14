21579eadb06eb2cc5c4f77cf0f166a59aa2e7094
python <younext@126.com>
2019-05-14 15:10:16 +0800

    Create reuseaddr.cpp

---
+++ reuseaddr.cpp
@@ -0,0 +1,24 @@
+
+socket tells the kernel that
+even if this port is busy(in the TIME_WAIT state), go ahead and reuse it anyway.
+if it is busy, but with another state, you will still get an address already in use error.
+
+int on = 1;
+setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (char*)&on, sizeof(on));
+after set this option, when one port is in TIME_WAIT state, we can bind to this port again with other five tuple.
+
+1.set this time socket to SO_REUSEADDR
+2.this time can reuse this port
+
+
+
+
+
+
+
+
+
+
+
+
+
