0d36a2148ffa17dbc76fb9a2d93c8ac46f30e761
python <younext@126.com>
2019-05-15 12:29:40 +0800

    Update reuseaddr.cpp

--- reuseaddr.cpp
+++ reuseaddr.cpp
@@ -10,9 +10,19 @@ after set this option, when one port is in TIME_WAIT state, we can bind to this 
 1.set this time socket to SO_REUSEADDR
 2.this time can reuse this port
 
+/---------------------------------------------------------------------------------------------------------------------/
+SO_REUSEADDR
+after test:
+    whatever set/noset, can not bind specify ip/port and INADDR_ANY/port (Linux cmad-android-dailybuild 2.6.32-33)
+    if set reuse, can bind to ip/port when it is TIME_WAIT
+    
+SO_REUSEPORT
+    allow to more than one socket which set SO_REUSEPORT bind to smae ip/port 
+    if first one do not set SO_REUSEPORT, later socket will fail when bind.
 
-
-
+SO_LINGER
+    if set SO_LINGER, when call close function TCP/IP stack will drop data in buffer and send RST
+    
 
 
 
